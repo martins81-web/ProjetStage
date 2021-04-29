3c3bfca98d41906cd8e9da4f6e69f5c557ad8427
Charles-Etienne Villemure <charles-etienne.villemure@hotmail.com>
2021-04-28 21:40:25 -0400

    Ajout carte contact liste-tache-etudiant

--- src/app/pages/ListeTachesEtudiant.js
+++ src/app/pages/ListeTachesEtudiant.js
@@ -3,6 +3,9 @@ import { useSubheader } from '../../_metronic/layout';
 import { InformationsGroupe } from '../components/InformationsGroupe';
 import { ApercuStagiaires } from '../components/ApercuStagiaires';
 import { ApercuTachesEtudiant } from '../components/cards/ApercuTachesEtudiant';
+import { CardStagiaireContact } from '../components/cards/CardStagiaireContact';
+import { CardTuteur } from '../components/cards/CardTuteur';
+import { CardEntrepriseContact } from '../components/cards/CardEntrepriseContact';
 import { Row, Col, Button } from 'react-bootstrap';
 export const ListeTachesEtudiant = () => {
   const suhbeader = useSubheader();
@@ -24,7 +27,15 @@ export const ListeTachesEtudiant = () => {
             <Button className='mb-3'>Voir plus</Button>
           </center>
         </Col>
-        <Col xs='2' className='bg-white'></Col>
+        {/* className PX-4 est pour sajuster avec la card tuteur */}
+        <Col xs='2' className='bg-white'>
+          <h1 className='text-muted px-4 mt-5'>Profil</h1>
+          <CardStagiaireContact />
+          <h1 className='text-muted px-4 mt-5 mb-0'>Tuteur</h1>
+          <CardTuteur />
+          <h1 className='text-muted px-4'>Hote</h1>
+          <CardEntrepriseContact />
+        </Col>
       </Row>
     </>
   );
